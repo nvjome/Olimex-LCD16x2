38736168df1f971ebdf33d8165fc4de21fdafa3a
nvjome <94138462+nvjome@users.noreply.github.com>
2023-06-19 11:07:36 -0500

    Update OLIMEX_LCD16x2.h

--- src/OLIMEX_LCD16x2.h
+++ src/OLIMEX_LCD16x2.h
@@ -47,44 +47,41 @@
  * @version     1.0
  * @since       2013-11-03
  */
-class LCD16x2{
-    public:
-        
-        LCD16x2();
-        ~LCD16x2();
+class LCD16x2: publis Print{
+  public:
+    LCD16x2();
+    ~LCD16x2();
 
-        void begin();
-        
-        uint8_t getID();
-        uint8_t getFirmwareVersion();
-        
-        void uartEnable(bool state);
-        
-        void pinMode(uint8_t pin, uint8_t direction);
-        void digitalWrite(uint8_t pin, uint8_t level);
-        uint8_t digitalRead(uint8_t pin);
-        uint8_t readButtons();    
-        
-		void lcdSetBacklight(uint8_t value);
-        void lcdClear();
-        void lcdGoToXY(uint8_t x, uint8_t y);
-        void lcdSetCursor(uint8_t x, uint8_t y) {lcdGoToXY(x, y);}
-        void lcdWrite(char *string);
-        void lcdWrite(int intVal);
-		void lcdWrite(float floatVal, uint8_t precision);
-        
-        const static uint8_t GPIO1 = 1;
-        const static uint8_t GPIO2 = 2;
-        const static uint8_t GPIO3 = 3;
-        const static uint8_t GPIO4 = 4;
-        const static uint8_t GPIO5 = 5;
-        const static uint8_t GPIO6 = 6;
-        const static uint8_t GPIO7 = 7;
-        const static uint8_t GPIO8 = 8;
-        const static uint8_t GPIO9 = 9;        
-        
-    private:
-        uint8_t X;
-        uint8_t Y;
+    void begin();
+    uint8_t getID();
+    uint8_t getFirmwareVersion();
+    void uartEnable(bool state);
+
+    void pinMode(uint8_t pin, uint8_t direction);
+    void digitalWrite(uint8_t pin, uint8_t level);
+    uint8_t digitalRead(uint8_t pin);
+    uint8_t readButtons();    
+
+    void lcdSetBacklight(uint8_t value);
+    void lcdClear();
+    void lcdGoToXY(uint8_t x, uint8_t y);
+    void lcdSetCursor(uint8_t x, uint8_t y) {lcdGoToXY(x, y);}
+    void lcdWrite(char *string);
+    void lcdWrite(int intVal);
+    void lcdWrite(float floatVal, uint8_t precision);
+
+    const static uint8_t GPIO1 = 1;
+    const static uint8_t GPIO2 = 2;
+    const static uint8_t GPIO3 = 3;
+    const static uint8_t GPIO4 = 4;
+    const static uint8_t GPIO5 = 5;
+    const static uint8_t GPIO6 = 6;
+    const static uint8_t GPIO7 = 7;
+    const static uint8_t GPIO8 = 8;
+    const static uint8_t GPIO9 = 9;        
+
+  private:
+    uint8_t X;
+    uint8_t Y;
 };
 #endif
